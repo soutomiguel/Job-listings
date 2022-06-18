368dfb471c09c945c1664d4d08a139eb26574252
Miguel Souto <miguel.alejandro.souto@gmail.com>
2022-06-18 17:59:56 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+class jobItem{
+    constructor(
+        company,
+        logo,
+        newPost,
+        position,
+        role,
+        level,
+        postedAt,
+        contract,
+        locationJob,
+        languages,
+        tools
+    ){
+        this._company = company
+        this._logo = logo
+        this._newPost = newPost
+        this._position = position
+        this._role = role
+        this._level = level
+        this._postedAt = postedAt
+        this._contract = contract
+        this._locationJob = locationJob
+        this._languages = languages
+        this._tools = tools
+    }
+}
+
+const jobPost = new jobItem("", "", "", "", "", "", "", "", "", "", "",)
+
+
+
